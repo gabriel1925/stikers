188895e56ba8333233c147213bb04e889541fb49
gabriel1925 <pocholo_1925@hotmail.com>
2021-07-13 17:31:57 -0300

    inicial

--- src/routes/provedores.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const provedores = require('../controllers/provedores.controllers')
-const { isAuthenticated } = require('../helpers/auth')
-const router = express.Router()
-
-router.post("/cargarProvedores",isAuthenticated,provedores.cargarProvedores)
-
-module.exports = router
