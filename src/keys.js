745c54658ea435a860c8710baa3c350871dcb782
gabriel1925 <pocholo_1925@hotmail.com>
2022-01-07 19:19:17 -0300

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
     database:{
-        URI:'mongodb://localhost/inventario',
+        URI:'mongodb://URL MONGO/inventario',
         URIse:{
-            host: '192.168.0.11',
-            port:3307,
+            host: '',
+            port:00,
             dialect: 'mysql',
             pool: {
                 max: 5,
@@ -11,20 +11,9 @@ module.exports = {
                 acquire: 30000,
                 idle: 10000
               }},
-        DB:'bitnami_wordpress',
-        User:'desarrollo',
-        Password:'mercusys123'
-        // URIse:{
-        //     host: 'localhost',
-        //     dialect: 'mariadb',
-        //     pool: {
-        //         max: 5,
-        //         min: 0,
-        //         acquire: 30000,
-        //         idle: 10000
-        //       }},
-        // DB:'anida',
-        // User:'siriux',
-        // Password:'siriux'
+        DB:'',
+        User:'',
+        Password:''
+
     }
 }
